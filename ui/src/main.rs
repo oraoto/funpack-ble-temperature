a7f0d03cc1b4d015da6436e5dc0173b19f6e9673
Oraoto <oraoto@outlook.com>
2024-02-27 22:02:18 +0800

    ui: update window dimension

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -30,7 +30,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ui = UI::new(rx);
 
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([600.0, 400.0]),
         default_theme: eframe::Theme::Light,
         ..Default::default()
     };
